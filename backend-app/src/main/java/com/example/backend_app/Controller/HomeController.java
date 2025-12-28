6b54f6ec9c77d9d2c663ab1ab59dec11b5d70905
hrishikesh khot <hrishikeshkhot33@gmail.com>
2025-12-28 11:14:23 +0530

    Added Crossorigin

--- backend-app/src/main/java/com/example/backend_app/Controller/HomeController.java
+++ backend-app/src/main/java/com/example/backend_app/Controller/HomeController.java
@@ -3,9 +3,12 @@ package com.example.backend_app.Controller;
 
 import com.example.backend_app.Service.HomeService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+
+@CrossOrigin(origins = "*")
 @RestController
 public class HomeController {
 
